a64116a837be05d3a8320623fd7ad664365c18f0
jaco souza <jacoprogramador13@gmail.com>
2026-06-03 11:20:08 -0300

    aula 11, aula 12, projeto de sistema de biblioteca

---
+++ LOPAL-(Logica de programação em Python)/1°Projetos/SISTEMA_BIBLIOTECA.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+from tkinter import messagebox, ttk
+
+
+def cadastro_conta():
+    nome_usuario = usuario_nome.get()
+    cadastro_cpf_usuario = usuario_cpf_usuario.get()
+    cadastro_email_usuario = usuario_cadastro_email.get()
+    naoaluno_aluno_usuario = usuario_nao_aluno.get()
+    
+
+    if nome_usuario == "":
+        messagebox.showwarning("Aviso", "Por favor digite o nome da pessoa cadastrada!")
+    
+    elif cadastro_email_usuario == "":
+        messagebox.showwarning("Aviso", "Por favor digite o email da pessoa cadastrada!")
+    
+    elif cadastro_cpf_usuario == "":
+        messagebox.showwarning("Aviso!", "Por favor digite o cpf da pessoa cadastrada")
+
+    else:
+        messagebox.showinfo("Cadrasto feito com sucesso!", f" {nome_usuario}!, de {idade_usuario} anos logando no sistema!")
+    
+
+# Janela_principal
+
+janela_inicial = tk.Tk()
+janela_inicial.title("Cadastro do Usuário")
+janela_inicial.geometry("600x700")
+
+# Labels
--- LOPAL-(Logica de programação em Python)/JACÓ(¨_¨)/Aula12/01.py
+++ LOPAL-(Logica de programação em Python)/JACÓ(¨_¨)/Aula12/01.py
@@ -29,17 +29,22 @@ def segunda_janela():
     segunda_janela.title("Segunda Janela")
     segunda_janela.geometry("500x500")
 
-    lbl_segunda_janela = tk.Label(segunda_janela, text=("Bem-Vindo a Segunda Janela :)"), font=("Arial", 12), fg="blue")
+    lbl_segunda_janela = tk.Label(segunda_janela, text="Bem-Vindo a Segunda Janela :), font=('Arial', 12), fg='blue'")
     lbl_segunda_janela.grid(row=0, column=0, pady=10, padx=10)
 
 # Componentes
 # Labels
 lbl_mensagem_usuario = tk.Label(janela_bemvindo, text="Digite seu nome :)")
 lbl_mensagem_usuario.grid(row=0, column=0, pady=10, padx=10)
+
 lbl_mensagem_idade = tk.Label(janela_bemvindo, text="Digite sua idade :)")
 lbl_mensagem_idade.grid(row=1, column=0, pady=30, padx=10)
+
 lbl_mensagem_pais = tk.Label(janela_bemvindo, text ="Escolha seu país :)")
 lbl_mensagem_pais.grid(row=2, column=0, pady=30, padx=10)
+
+lbl_segunda_janela = tk.Label(janela_bemvindo, text="Clique para abrir a segunda janela :)")
+lbl_segunda_janela.grid(row=2, column=0, pady=10, padx=10)
 
 # Entrys
 usuario_nome = tk.Entry(janela_bemvindo, font=("Arial", 12), width=20)
@@ -55,8 +60,13 @@ combo_nivel.grid(row=2, column=1, pady=10, padx=10)
 # Botão
 btn_enviar_mensagem = tk.Button(janela_bemvindo, text="Enviar Mensagem", command=bemvindo)
 btn_enviar_mensagem.grid(row=3, column=0, pady=20, padx=30)
+
 btn_clicar_fechar = tk.Button(janela_bemvindo, text="Fechar Aplicativo", font=("Arial", 14), bg="#a30000" , fg="white", command=janela_bemvindo.destroy)
 btn_clicar_fechar.grid(row=5, column=5, pady=350, padx=20) 
 
+btn_segunda_janela = tk.Button(janela_bemvindo, text="Abrir Segunda Janela", command=segunda_janela)
+btn_segunda_janela.grid(row=3, column=4, pady=10, padx=10)
+
 # Rodar interface
 janela_bemvindo.mainloop()
+segunda_janela.mainloop()
